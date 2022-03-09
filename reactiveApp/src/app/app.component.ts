adbcc4f3bd68455803c02113bb01da8fa67ac776
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2022-03-09 23:06:51 +0600

    first commit

---
+++ reactiveApp/src/app/app.component.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  frmData: FormGroup;
+
+  SelectedfavouriteGameIdList: Array<any> = [];
+  uploadedFiles: string[] = [];
+  favouriteGames: Array<any> = [
+    { id: 1, name: 'FootBall', isActive: true },
+    { id: 2, name: 'Cricket', isActive: true },
+    { id: 3, name: 'Hocky', isActive: false },
+    { id: 4, name: 'Tenis', isActive: true },
+    { id: 5, name: 'BolyBall', isActive: false },
+    { id: 6, name: 'Kabady', isActive: true },
+  ];
+
+  Groups: Array<any> = [
+    { id: 1, name: 'Science', isActive: false },
+    { id: 2, name: 'Commerce', isActive: true },
+    { id: 3, name: 'Atrs', isActive: false },
+  ];
+
+  Countries: Array<any> = [
+    { id: 1, name: 'Bangladesh' },
+    { id: 2, name: 'England' },
+    { id: 3, name: 'India' },
+    { id: 4, name: 'Pakisthan' },
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.frmData = new FormGroup({
+      Name: new FormControl('', [Validators.required]),
+      DateOfBirth: new FormControl('', Validators.required),
+      favouriteGame: new FormControl('', Validators.required),
+      group: new FormControl('', Validators.required),
+      Country: new FormControl('', Validators.required),
+      file: new FormControl('', Validators.required),
+    });
+  }
+
+  get f() {
+    return this.frmData.controls;
+  }
+
+  onFileChange(event) {
+    for (var i = 0; i < event.target.files.length; i++) {
+      this.uploadedFiles.push(event.target.files[i]);
+      
+    }
+  }
+
+  onCheckboxChange(id,name, event) {
+    if (event.target.checked) {
+      this.SelectedfavouriteGameIdList.push({Id:id,Name:name});
+    } else {
+      for (var i = 0; i < this.favouriteGames.length; i++) {
+        if (this.SelectedfavouriteGameIdList[i] == id) {
+          this.SelectedfavouriteGameIdList.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  SaveData() {
+    
+
+    const formData = new FormData();
+
+    for (var i = 0; i < this.uploadedFiles.length; i++) {
+      formData.append('file[]', this.uploadedFiles[i]);
+    }
+    console.log(this.frmData.value);
+    console.log(this.SelectedfavouriteGameIdList);
+    console.log(this.uploadedFiles);
+  }
+}
---
+++ reactiveApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
